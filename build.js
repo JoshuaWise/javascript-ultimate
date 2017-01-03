9a4489849adde519e39119ae7b9694113086205a
Joshua Wise <joshuathomaswise@gmail.com>
2017-01-02 19:36:14 -0500

    added dom version

--- build.js
+++ build.js
@@ -1,11 +1,13 @@
 'use strict';
 const fs = require('fs');
+const path = require('path');
 const glob = require('glob').sync;
 const yaml = require('js-yaml');
 const Mixin = require('./lib/mixin');
-const mixins = glob('./mixins/*.yaml').map(filename => new Mixin(filename));
+const applyVersion = require('./lib/apply-version');
+const sourceFilename = glob('./*.sublime-syntax.source')[0];
 
-const getMixin = (name) => {
+const getMixin = (mixins, name) => {
 	const mixin = mixins.find(mixin => mixin.name === name);
 	if (!mixin) {
 		throw new ReferenceError(`Unrecognized mixin "${name}".`);
@@ -24,19 +26,29 @@ const walkMixins = (obj, fn) => {
 	return count;
 };
 
-glob('./*.sublime-syntax.source').forEach(function process(filename) {
-	const file = yaml.safeLoad(fs.readFileSync(filename).toString());
-	const count = walkMixins(file.contexts, (def, key, obj) => {
-		const mixin = getMixin(def.mixin);
-		def.name = key;
-		delete def.mixin;
-		delete obj[key];
-		const results = yaml.safeLoad(mixin.render(def));
-		for (let context in results) {
-			obj[context] = results[context];
-		}
-	});
-	filename = filename.replace(/\.source$/g, '');
-	fs.writeFileSync(filename, '%YAML 1.2\n---\n' + JSON.stringify(file, null, '    '));
-	count && process(filename);
+glob('./versions/*.yaml').forEach(filename => {
+	let versionName = path.basename(filename, path.extname(filename));
+	const versionLibrary = yaml.safeLoad(fs.readFileSync(filename).toString());
+	const mixins = glob('./mixins/*.yaml').map(filename => new Mixin(filename, versionLibrary));
+	if (versionName === 'default') {
+		versionName = '';
+	} else {
+		versionName = '.' + versionName;
+	}
+	(function process(filename) {
+		const file = yaml.safeLoad(applyVersion(versionLibrary, fs.readFileSync(filename).toString()));
+		const count = walkMixins(file.contexts, (def, key, obj) => {
+			const mixin = getMixin(mixins, def.mixin);
+			def.name = key;
+			delete def.mixin;
+			delete obj[key];
+			const results = yaml.safeLoad(mixin.render(def));
+			for (let context in results) {
+				obj[context] = results[context];
+			}
+		});
+		filename = filename.replace(/\.sublime-syntax\.source$/g, versionName + '.sublime-syntax');
+		fs.writeFileSync(filename, '%YAML 1.2\n---\n' + JSON.stringify(file, null, '    '));
+		count && process(filename);
+	}(sourceFilename));
 });
